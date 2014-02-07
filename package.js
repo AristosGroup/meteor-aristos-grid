a5aabf3991d7b3ce944efb3b209f96bd0afd3410
Aleksey Kuznetsov <mrakobesov@gmail.com>
2014-02-07 19:26:23 +0400

    fix oplog issue

--- package.js
+++ package.js
@@ -3,8 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-  api.use('underscore', ['client', 'server']);
-  api.use('jqwidgets', ['client']);
+  api.use(['underscore','deps'], ['client', 'server']);
+  api.use(['jqwidgets'], ['client']);
 
   api.add_files(['lib/grid.js']);
 });

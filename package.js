8dac8ec10f60b84904314284bb7839c6a26d5761
ProWebCraft <prowebcraft@gmail.com>
2014-03-20 17:51:12 +0400

    Модуль вывода таблиц с данными

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
-  summary: "Aristos-base"
+  summary: "Grid module based on jqWidgets.Grid"
 });
 
 Package.on_use(function(api) {
+  api.use(['iron-router'], 'server');
   api.use(['underscore','deps'], ['client', 'server']);
-  api.use(['jqwidgets'], ['client']);
+  api.use(['jqwidgets'], 'client');
 
+  api.add_files(['lib/export_script.js'], 'server');
   api.add_files(['lib/grid.js']);
 });

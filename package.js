c37377270a57e24d227cf02a36437b2546d7904e
ProWebCraft <prowebcraft@gmail.com>
2014-03-25 17:08:19 +0400

    Готовый модуль для запросов в Яндекс.API (без выгрузки) Фильтрация коллекций на стороне сервера

--- package.js
+++ package.js
@@ -1,12 +1,14 @@
 Package.describe({
-  summary: "Grid module based on jqWidgets.Grid"
+    summary: "Grid module based on jqWidgets.Grid"
 });
 
-Package.on_use(function(api) {
-  api.use(['iron-router'], 'server');
-  api.use(['underscore','deps'], ['client', 'server']);
-  api.use(['jqwidgets'], 'client');
+Package.on_use(function (api) {
+    api.use(['iron-router'], 'server');
+    api.use(['underscore', 'deps'], ['client', 'server']);
+    api.use(['templating', 'handlebars', 'spark', 'session'], 'client');
+    api.use(['jqwidgets'], 'client');
 
-  api.add_files(['lib/export_script.js'], 'server');
-  api.add_files(['lib/grid.js']);
+    api.add_files(['lib/export_script.js'], 'server');
+    api.add_files(['lib/grid.js']);
+    api.add_files(['client/grid.html', 'client/states.html', 'client/states.js'], 'client');
 });
